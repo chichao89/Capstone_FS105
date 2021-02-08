f6fd4b11b4e33070bc9dee73f726656a097e2e05
chichao89 <70385670+chichao89@users.noreply.github.com>
2021-02-08 19:08:47 +0800

    "added all the api views"

--- CapstoneBackend/capstone_proj/testimonial/views.py
+++ CapstoneBackend/capstone_proj/testimonial/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Testimonial
+from .serializers import TestimonialSerializer
 # Create your views here.
+
+class TestimonialViewSet(viewsets.ModelViewSet):
+    queryset = Testimonial.objects.all()
+    serializer_class = TestimonialSerializer
