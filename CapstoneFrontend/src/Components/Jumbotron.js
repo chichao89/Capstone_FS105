f951560561b5a14906e5a34ae2928303aa6935d7
abhisekrai88 <kesihba.iar@gmail.com>
2021-02-15 16:07:56 +0800

    jumbo background and typical edits

--- CapstoneFrontend/src/Components/Jumbotron.js
+++ CapstoneFrontend/src/Components/Jumbotron.js
@@ -10,21 +10,21 @@ const Jumbotron = () => {
       <Container>
         <div className="col-lg-8 text-left">
         {/*<h6 className="text-white text-uppercase">Now you can feel the energy</h6>*/}
-        <h1 className="display-2 text-warning">Hi, I'm Dee. How can i help? 
+        <h1 className="display-6 text-warning">Hi, I'm Dee and I'm a home-based manicurist. How can i help? 
         <Typical
             loop={Infinity}
             wrapper='p'
             steps={[
                 ' Apres Extensions?',
-                2000,
+                4000,
                 ' Acrylic Extensions?',
                 4000,
                 ' What about PolyGel Extensions?',
-                6000,
+                4000,
                 ' No? How about good old Gel Manicure?',
-                8000,
+                4000,
                 ' Wanna talk about it instead?',
-                10000
+                4000
             ]}/>
                 </h1>
         <h5 className="text-white">Look me up! I can customise something for your needs.</h5>
