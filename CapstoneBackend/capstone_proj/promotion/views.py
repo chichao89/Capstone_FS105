52441ad4b244b200611fd1095cb3232e485ab971
abhisekrai88 <kesihba.iar@gmail.com>
2021-02-23 00:03:32 +0800

    minor edits to jumbo. success calling promo img

--- CapstoneBackend/capstone_proj/promotion/views.py
+++ CapstoneBackend/capstone_proj/promotion/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Promotion
 from .serializers import PromotionSerializer
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
 class PromotionViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = Promotion.objects.all()
     serializer_class = PromotionSerializer
 # Create your views here.
