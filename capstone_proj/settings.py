16d0d50c1eab9204b51bd718df182ef4d352d50f
chichao89 <70385670+chichao89@users.noreply.github.com>
2021-03-09 11:27:43 +0800

    "update settings.py"

--- capstone_proj/settings.py
+++ capstone_proj/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '$lje)rho*yt)5wi*cxrgjjk8__xmd50wflzm+iw3-=!sfm*i-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
